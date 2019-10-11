47854b57134eeeb3f733bf21b1f698747a3c7709
vegtamr <barekov.am.v@gmail.com>
2019-10-11 10:49:43 +0300

    Added individual task.

---
+++ diamond.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+INVARGS="Invalid arguments. Aborting..."
+
+modula=$(($1 % 2))
+halved=$(($1 / 2 + 1))
+
+if [[ -z $1 ]] || [[ $(echo "$1 % 2" | bc) -eq 0 ]];
+then
+  echo "$INVARGS"
+  exit
+fi
+
+spaces=1
+for ((i = 1; i <= $halved; i++))
+do
+  str=""
+  j=$(($(echo "$1 - $spaces" | bc) / 2))
+  for ((k = 1; k <= $j; k++))
+  do
+    str=$str"p"
+  done
+
+  for ((k = 1; k <= $i; k++))
+  do
+    str=$str"*p"
+  done
+
+  echo $str | sed 's/p/ /g'
+  spaces=$(($spaces + 2))
+done
+
+spaces=$(($spaces - 4))
+
+for ((i = $(($halved + 1)); i <= $1; i++))
+do
+  str=""
+  j=$(($(echo "$1 - $spaces" | bc) / 2))
+  for ((k = 1; k <= $j; k++))
+  do
+    str=$str"p"
+  done
+
+  for ((k = 1; k <= $(($1 - $i + 1)); k++))
+  do
+    str=$str"*p"
+  done
+
+  echo $str | sed 's/p/ /g'
+  spaces=$(($spaces - 2))
+done
